fd04328bfcc43e3a9d9b6b8dda242b8cbce6bd93
Gabazevdo <gbldja@gmail.com>
2022-06-27 16:32:54 -0300

    LÓGICA: validação dos campos de inputs

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,43 @@
 import loginUser from './requisicoes/loginUser.js'
 
+import { limparValorDeObjetos } from '../constants/limparCampos.js'
+
+
 // Selecionando o botão para fazer login
 const loginUserButtonElement = document.querySelector('#loginUserButton')
 
+const inputElement = document.querySelectorAll('.form-control')
 
-// Dado mocado para realizar teste de login. Esse usuário e senha foram cadastrados. 
-
-let objetoUsuario = {
-  email: '123@ab.com',
-  password: '12345678'
+// Dado mocado para realizar teste de login. Esse usuário e senha foram cadastrados.
+let formValidationLogin = {
+  email: false,
+  password: false
 }
-
 
 const index = () => {
+  for (let control of inputElement) {
+    const controlInputElement = control.children[1]
 
-  loginUserButtonElement.addEventListener('click', event => {
-    event.preventDefault()
+    controlInputElement.addEventListener('keyup', event => {
+      let inputValid = event.target.checkValidity()
+      let inputValue = event.target.value
 
-    loginUser(objetoUsuario);
-
-  })
+      if (inputValid) {
+        formValidationLogin[event.target.id] = inputValid
+        formValidationLogin[event.target.id] = inputValue
+        // Para deixar o botão ativado
+        loginUserButtonElement.disabled = false
+      }
+    })
+  }
 }
 
-export default index
+loginUserButtonElement ?
+loginUserButtonElement.addEventListener('click', event => {
+  event.preventDefault()
+  loginUser(formValidationLogin)
+  limparValorDeObjetos(inputElement)
 
+}): ''
+
+export default index
