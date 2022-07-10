be48d310b7806a61078689ce0106bf1658d62ddd
Gabazevdo <gbldja@gmail.com>
2022-07-10 10:06:23 -0300

    LÓGICA: alertas de acordo com o retorno do server

--- scripts/requisicoes/createUser.js
+++ scripts/requisicoes/createUser.js
@@ -3,19 +3,18 @@
 import { BASE_URL } from '../../constants/base_url.js'
 //Importando cabeçaho para API
 import requestHeader from '../../constants/headerRequest.js'
-// IMportando o loading 
+// IMportando o loading
 import { loading } from '../../constants/loading.js'
-
 
 //------------------------------------------- INÍCIO FUNÇÃO PARA CONTROLAR E ENVIAR OS DADOS PARA A API
 const createUser = (dadosObjetoUsuario, botaoCriaUsuario) => {
-
   let requestConfiguration = {
     method: 'POST',
     headers: requestHeader,
     body: JSON.stringify(dadosObjetoUsuario)
   }
 
+  
   fetch(`${BASE_URL}/users`, requestConfiguration).then(response => {
     response.json().then(info => {
       loading()
@@ -34,21 +33,47 @@ const createUser = (dadosObjetoUsuario, botaoCriaUsuario) => {
             //Condiçõ para o usuário ir diretamente para as tarefas
             localStorage.setItem('token', info.jwt)
             window.location = '/tarefas.html'
-          }
-          else{
+          } else {
             window.location.reload()
           }
         })
+      }
+      if (info === 'El usuario ya se encuentra registrado') {
+        Swal.fire({
+          icon: 'warning',
+          title: 'Oops...',
+          text: 'Usuário já cadastrado!'
+        })
+        botaoCriaUsuario.disabled = true
+      }
 
-      } else {
-        if (info === 'El usuario ya se encuentra registrado') {
-          Swal.fire({
-            icon: 'warning',
-            title: 'Oops...',
-            text: 'Usuário já cadastrado!'
-          })
-          botaoCriaUsuario.disabled = true
-        }
+      if (response.status >= 400 && response.status <= 499) {
+        Swal.fire({
+          position: 'center',
+          icon: 'error',
+          title: 'Houve um erro!',
+          text: 'Verifique o console',
+
+        })
+        console.log(`
+        ${info}
+        ${response.statusText}`)
+        botaoCriaUsuario.disabled = true
+
+      }
+
+      if (response.status === 500) {
+        Swal.fire({
+          position: 'center',
+          icon: 'error',
+          title: 'Estamos com problemas no servidor',
+          text: 'Desculpe o transtorno, tente novamente em instantes.'
+        })
+        console.log(`
+        ${info}
+        ${response.statusText}`)
+        botaoCriaUsuario.disabled = true
+
       }
     })
   })
