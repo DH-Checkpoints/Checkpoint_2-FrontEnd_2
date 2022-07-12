8a8a187f360dcfca1edd6a96e8999c99e394ee2a
Gabazevdo <gbldja@gmail.com>
2022-07-11 23:01:27 -0300

    LÓGICA: alterando o link da API

--- constants/base_url.js
+++ constants/base_url.js
@@ -1,4 +1,4 @@
-export const BASE_URL = `https://ctd-todo-api.herokuapp.com/v1`
+export const BASE_URL = 'https://ctd-fe2-todo-v2.herokuapp.com/v1'
 
 // link api: https://ctd-todo-api.herokuapp.com/
 // base url: https://ctd-todo-api.herokuapp.com/v1
